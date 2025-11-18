a5f4a92ad5c801f9cb16056e2b559cae26169580
jskway <jack@jackkim.dev>
2025-11-18 01:10:00 -0800

    Update for 3rd edition

--- 4-functions/zigzag.py
+++ 4-functions/zigzag.py
@@ -1,6 +1,6 @@
 import time, sys
 indent = 0 # How many spaces to indent.
-indentIncreasing = True # Whether the indentation is increasing or not.
+indent_increasing = True # Whether the indentation is increasing or not.
 
 try:
     while True: # The main program loop.
@@ -8,18 +8,18 @@ try:
         print('********')
         time.sleep(0.1) # Pause for 1/10 of a second.
 
-        if indentIncreasing:
+        if indent_increasing:
             # Increase the number of spaces:
             indent = indent + 1
             if indent == 20:
                 # Change direction:
-                indentIncreasing = False
+                indent_increasing = False
 
         else:
             # Decrease the number of spaces:
             indent = indent - 1
             if indent == 0:
                 # Change direction:
-                indentIncreasing = True
+                indent_increasing = True
 except KeyboardInterrupt:
     sys.exit()
